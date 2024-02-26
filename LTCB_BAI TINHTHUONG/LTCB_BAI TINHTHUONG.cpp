553b5fa1f555e096b909f7aa5528dde1e534cdc0
ADMIN <ADMIN@LAB1-MAY09>
2024-02-26 08:59:35 +0700

    Add project files.

---
+++ LTCB_BAI TINHTHUONG/LTCB_BAI TINHTHUONG.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int so1, so2;
+	float thuong;
+	cout << "CHUONG TRINH TINH THUONG\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+	if (so2 != 0) {
+		thuong = (float) so1 / so2;
+		cout << so1 << "/" << so2 << "=" << thuong << endl;
+	}else {
+		cout << " khong the chia" << so1 << "cho 0\n";
+	}
+	return 0;
+}
